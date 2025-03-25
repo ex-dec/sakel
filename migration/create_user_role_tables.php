f1085d3d285ef15d521a50022d8466707951e00d
ex-dec <bighadi123@yahoo.com>
2025-03-25 23:40:27 +0700

    fix(migration): add pdo class init

--- migration/create_user_role_tables.php
+++ migration/create_user_role_tables.php
@@ -1,6 +1,8 @@
 <?php
 require_once __DIR__ . '/../config/database.php';
 
+$pdo = Database::connect();
+
 $pdo->exec("CREATE TABLE IF NOT EXISTS roles (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL
