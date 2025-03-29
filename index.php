4bb2c24b34046019da4a60752a5defba49b72d32
ex-dec <bighadi123@yahoo.com>
2025-03-29 14:39:14 +0700

    feat(public): change public directory to root dir

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
-$router = require_once __DIR__ . '/../routes/web.php';
+$router = require_once __DIR__ . '/routes/web.php';
 $router->dispatch($method, $path);
